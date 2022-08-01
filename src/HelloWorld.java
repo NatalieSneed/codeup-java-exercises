7c5566fb3f3814a9badd7aadb9ecbac170b4269a
NatalieSneed <106278703+NatalieSneed@users.noreply.github.com>
2022-08-01 16:56:28 -0500

    HelloWorld exercise complete

---
+++ src/HelloWorld.java
@@ -0,0 +1,43 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+        System.out.println("Hello, World");
+
+        int myFavoriteNumber = 5;
+        System.out.println("My Favorite Number = " + myFavoriteNumber);
+
+        String myString = "Stringing Along";
+        System.out.println(myString);
+
+        float myNumber = (float) 3.1414;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+//        int x = 4;
+//        System.out.println(x += 5);
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(y * x);
+
+            int x = 10;
+            int y = 2;
+            x = x / y;
+            y = y - x;
+            System.out.println(x);
+
+    }
+}
+
+
