cc001effcae9c270db50e287799f913409dc0cc0
NatalieSneed <106278703+NatalieSneed@users.noreply.github.com>
2022-08-23 12:06:22 -0500

    updates made to shapes/Rectangle specifically

---
+++ src/shapes/Quadrilateral.java
@@ -0,0 +1,9 @@
+package shapes;
+
+public abstract class Quadrilateral {
+    public abstract double getPerimeter();
+
+    public abstract double getArea();
+
+    public abstract void setWidth(int width);
+}
--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,15 +1,47 @@
 package shapes;
 
-public class Rectangle extends Quadrilateral {
-    //    protected properties for length & width
-    protected double length;
-    protected double width;
+//rectangle class inside the shapes package
+public class Rectangle extends Quadrilateral implements Measurable {
+//    //    ->Inheritance & Polymorphism:
+//    //    protected properties for length & width
+//    protected int length;
+//    protected int width;
 
-    //    constructor that accepts 2 #'s for length & width
-    public Rectangle(double length, double width) {
-        System.out.println(length);
-        this.length = length;
-        System.out.println(length);
-        this.width = width;
+    //    constructor that accepts 2 #'s for length & width; & set those properties
+//    public Rectangle(int length, int width) {
+//        System.out.println(length);
+//        this.length = length;
+//        System.out.println(length);
+//        this.width = width;
+//    }
+//    create getArea method
+//    public int getArea() {
+//        int area = this.length * this.width;
+//        return area;
+//    }
+//create a getPerimeter method
+//    public int getPerimeter() {
+//        int perimeter = (this.length * 2) + (this.width * 2);
+//        return perimeter;
+//    }
+
+//Interfaces & Abstract Classes Exercise Methods
+
+    @Override
+public double getPerimeter() {
+    return 0;
+        }
+
+
+@Override
+public double getArea() {
+    return 0;
+        }
+        public void setLength(int length) {
+
+        }
+        @Override
+        public void setWidth(int width) {
+
+        }
     }
-}
